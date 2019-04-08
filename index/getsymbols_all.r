e53947c777256b442ba7f2989a126e303d98e4db
Hisayoshi Onomoto <honomoto@onomoto-nb3.local>
2019-04-08 10:11:22 +0900

    add auto.assign param

--- index/getsymbols_all.r
+++ index/getsymbols_all.r
@@ -7,25 +7,25 @@ gdp_g_r <- 1.04 # pesumed GDP growth rate
 i <- seq(2,len_mon/3,1)  # seq of quarters to predict
 d <- as.Date(as.yearqtr(seq(Sys.Date(),as.Date("2100-12-31"),by="quarters")[i])) # pick up the first day of each quarters.
 
-getSymbols("PAYEMS",src="FRED")
+getSymbols("PAYEMS",src="FRED",auto.assign=TRUE)
 PA <- PAYEMS
 m_PA <- as.xts(forecast(auto.arima(PA),h=len_mon)$mean[1:len_mon],as.Date(as.yearmon(mondate(index(last(PA)))+seq(1,len_mon,1)),frac=0))[as.Date(as.yearqtr(mondate(index(last(PA)))+seq(3,len_mon,3)),frac=0)]
 PAq <- apply.quarterly(PA[k2k],mean)
 length(PAq)
 
-getSymbols("UNDCONTSA",src="FRED")
+getSymbols("UNDCONTSA",src="FRED",auto.assign=TRUE)
 UC <- UNDCONTSA
 m_UC <- as.xts(forecast(auto.arima(UC),h=len_mon)$mean[1:len_mon],as.Date(as.yearmon(mondate(index(last(UC)))+seq(1,len_mon,1)),frac=0))[as.Date(as.yearqtr(mondate(index(last(UC)))+seq(3,len_mon,3)),frac=0)]
 UCq <- apply.quarterly(UC[k2k],mean)
 length(UCq)
 
-getSymbols('SPCS10RSA',src='FRED')
+getSymbols('SPCS10RSA',src='FRED',auto.assign=TRUE)
 CS <- SPCS10RSA
 m_CS_2012 <- as.xts(forecast(auto.arima(CS["2012::"]),h=len_mon)$mean[1:len_mon],as.Date(as.yearmon(mondate(index(last(CS)))+seq(1,len_mon,1)),frac=0))[as.Date(as.yearqtr(mondate(index(last(CS)))+seq(3,len_mon,3)),frac=0)]
 CSq <- apply.quarterly(CS[k2k],mean)
 length(CSq)
 
-getSymbols("GDP",src="FRED")
+getSymbols("GDP",src="FRED",auto.assign=TRUE)
 G <- GDP
 # m_GDP <- as.xts(as.vector(last(GDP)) * r**(i/4),d)
 m_GDP <- as.xts(as.vector(last(G)) * gdp_g_r**(seq(1,len_mon/3,1)/4),as.Date(as.yearqtr(mondate(index(last(G)))+seq(3,len_mon,3)),frac=0))
