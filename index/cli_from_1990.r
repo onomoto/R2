735b3f7e0659c762b2bb07c13e72521189bc41a1
onomoto <onomoto@me.com>
2019-04-10 15:27:32 +0900

    update rgb prameter to improve contrasts

--- index/cli_from_1990.r
+++ index/cli_from_1990.r
@@ -1,3 +1,3 @@
 hist(as.vector(VIX[as.Date(setdiff(seq(as.Date("1990-05-01"),as.Date("2019-01-01"),by='months') ,as.Date(index(na.trim(diff(cli_xts$oecd["1990::"],lag=4))[na.trim(diff(cli_xts$oecd["1990::"],lag=4)) < -0.2 ]))))][,4]),xlim=c(5,60),ylim=c(0,60),col=rgb(1,1,0,alpha=0.8),breaks = 10)
 par(new=T)
-hist(as.vector(VIX[as.Date(index(na.trim(diff(cli_xts$oecd["1990::"],lag=4))[na.trim(diff(cli_xts$oecd["1990::"],lag=4)) < -0.2 ]))][,4]),xlim=c(5,60),ylim=c(0,60),col=rgb(0,0,1,alpha=0.2),breaks=20)
+hist(as.vector(VIX[as.Date(index(na.trim(diff(cli_xts$oecd["1990::"],lag=4))[na.trim(diff(cli_xts$oecd["1990::"],lag=4)) < -0.2 ]))][,4]),xlim=c(5,60),ylim=c(0,60),col=rgb(0,0,1,alpha=0.4),breaks=20)
