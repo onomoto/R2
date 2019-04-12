366e0aae14e5e919d3829de3a9b58d75d1462cbc
onomoto <onomoto@me.com>
2019-04-12 10:35:05 +0900

    add code to generate seq. on the opposite condition.

--- index/cli_work_month.r
+++ index/cli_work_month.r
@@ -8,10 +8,17 @@ index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag
 #
 #  the list of months from 2000 till the updated cli.
 #
-seq(as.Date("2000-01-01"),paste("2000-01-01::",index(last(cli_xts)),sep=""),by='months')
+seq(as.Date("2000-01-01"),as.Date(index(last(cli_xts))),by='months')
 #
 # selct month from CY2000 where cli 6 mon. delta AND/OR 1 mon. delta is more than ZERO.
 #
-setdiff(seq(as.Date("2000-01-01"),paste("2000-01-01::",index(last(cli_xts)),sep=""),by='months'),
-index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0 & na.omit(diff(cli_xts$oecd["1999-12-01::"])) < 0])
-)
+as.Date(setdiff(seq(as.Date("2000-01-01"),as.Date(index(last(cli_xts))),by='months'),
+index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0 & na.omit(diff(cli_xts$oecd["1999-12-01::"])) < 0])))
+#
+# select month from CY2000 where cli 6 mon. delta is equal to or more than 0.
+#
+index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) >= 0])
+#
+#   OR
+#
+as.Date(setdiff(seq(as.Date("2000-01-01"),as.Date(index(last(cli_xts))),by='months'),index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0])))
