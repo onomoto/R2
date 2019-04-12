07e35ad3e4997b89dc981205a2bb1b2da50bfb83
onomoto <onomoto@me.com>
2019-04-12 13:42:34 +0900

    add code to check, when correction takes place, the month's 5 mon. and 1 mon. delta are less than ZERO.

--- index/cli_work_month.r
+++ index/cli_work_month.r
@@ -22,3 +22,21 @@ index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag
 #   OR
 #
 as.Date(setdiff(seq(as.Date("2000-01-01"),as.Date(index(last(cli_xts))),by='months'),index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0])))
+#
+#  check any month whose low is less than 90% of high belongs to those which cli 5 mon. delta and 1 mon. delta are less than ZERO. from 2007-01-01 to 2019-02-01
+#
+is.element(as.Date(index(to.monthly(GSPC)[,4][to.monthly(GSPC)[,3]/to.monthly(GSPC)[,2] < 0.9]))[12:18],index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=5)) < 0 & na.omit(diff(cli_xts$oecd["1999-12-01::"])) < 0]))
+#
+#  check any month whose low is less than 90% of high belongs to those which cli 5 mon. delta and 1 mon. delta are less than ZERO. from 2000-01-01 to 2019-02-01
+#
+index(SP5["2000-01-01::2019-02-01"][SP5[,3]["2000-01-01::2019-02-01"]/SP5[,2]["2000-01-01::2019-02-01"] < 0.9])
+#  [1] "2000-03-01" "2000-04-01" "2000-10-01" "2001-02-01" "2001-03-01" "2001-04-01" "2001-09-01"
+#  [8] "2002-06-01" "2002-07-01" "2002-08-01" "2002-09-01" "2002-10-01" "2003-01-01" "2003-03-01"
+# [15] "2008-01-01" "2008-09-01" "2008-10-01" "2008-11-01" "2008-12-01" "2009-01-01" "2009-02-01"
+# [22] "2009-03-01" "2009-04-01" "2009-07-01" "2010-05-01" "2011-08-01" "2011-10-01" "2015-08-01"
+# [29] "2016-01-01" "2018-02-01" "2018-10-01" "2018-12-01"
+is.element(index(SP5["2000-01-01::2019-02-01"][SP5[,3]["2000-01-01::2019-02-01"]/SP5[,2]["2000-01-01::2019-02-01"] < 0.9]),index(cli_xts$oecd["2000-01-01::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=5)) < 0 & na.omit(diff(cli_xts$oecd["1999-12-01::"])) < 0]))
+#  [1] FALSE FALSE  TRUE  TRUE  TRUE  TRUE  TRUE FALSE  TRUE  TRUE  TRUE  TRUE  TRUE  TRUE  TRUE  TRUE
+# [17]  TRUE  TRUE  TRUE  TRUE  TRUE FALSE FALSE FALSE FALSE  TRUE  TRUE  TRUE  TRUE  TRUE  TRUE  TRUE
+length( index(SP5["2000-01-01::2019-02-01"][SP5[,3]["2000-01-01::2019-02-01"]/SP5[,2]["2000-01-01::2019-02-01"] < 0.9]))
+# [1] 32
