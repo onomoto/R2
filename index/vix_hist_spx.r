6314130334d30bd70401294656c3431ed6deba97
onomoto <onomoto@me.com>
2019-04-16 17:03:50 +0900

    change a condition for "red" are to add 1 mon. delta < 0 as the new one.

--- index/vix_hist_spx.r
+++ index/vix_hist_spx.r
@@ -3,9 +3,9 @@
 
 # mnt <- index(cli_xts$oecd["2000::2019"][cli_xts$oecd["2000::2019"]/as.vector(cli_xts$oecd["1999-07-01::2018-08-01"]) < 1])
 
-mnt <- index(cli_xts$oecd["2000::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0])
-# plot.zoo(merge(VIX["2000::2019-02"][,4],VIX[mnt][,4]),type='h',col = c("red", "blue"), plot.type = "single")
-plot.zoo(merge(VIX[paste("2000::",substr(as.character(index(last(cli_xts))),1,7),sep="")][,4],VIX[mnt][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+mnt4 <- index(cli_xts$oecd["2000::"][na.omit(diff(cli_xts$oecd["1999-07-01::"],lag=6)) < 0 & na.omit(diff(cli_xts$oecd["1999-12-01::"])) < 0])
+# plot.zoo(merge(VIX["2000::2019-02"][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+plot.zoo(merge(VIX[paste("2000::",substr(as.character(index(last(cli_xts))),1,7),sep="")][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
 abline(v=seq(as.Date("2001-01-01"),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
 par(new=T)
 # plot.default(SP5[,4]["2000::2019-02"],axes=F,type='l')
