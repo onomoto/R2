9e8970fb74d555421fcb0b2e03e7618934ce6234
onomoto <onomoto@me.com>
2019-04-17 18:06:02 +0900

    adding event "85k"

--- bp/bp_plot.r
+++ bp/bp_plot.r
@@ -11,10 +11,10 @@
 len <- length(index(bp.xts[bp.xts$High > 95]))
 plot(bp.xts[,c(1,2)][bp.xts$High > 95],col = c("red", "blue"),lwd=c(3,3,2,2),
 major.ticks='days',grid.ticks.on='days',type='p',ylim=c(60,160))
-addSeries(merge(as.xts(rep(125,l),index(bp.xts[bp.xts$High > 95])),rep(75,l),
+addSeries(merge(as.xts(rep(125,len),index(bp.xts[bp.xts$High > 95])),rep(75,len),
      rep(mean(bp.xts[,1][bp.xts$High > 95]),len),
      rep(mean(bp.xts[,2][bp.xts$High > 95]),len)), ylim=c(60,160),on=1,col=4,lwd=1)
-events <- xts(c("natrix","weight","abort natrix","70k","75k","80k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15")))
+events <- xts(c("natrix","weight","abort natrix","70k","75k","80k","85k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
 addEventLines(events, srt=90, pos=2,col=10)
 axis(2,at=c(135,130,125,120,115,85,80,75,70))
 axis(4,at=c(135,130,125,120,115,85,80,75,70))
@@ -29,7 +29,7 @@ addSeries(merge(as.xts(rep(mean(bp.xts[,1][bp.xts$High > 95]),len),last(index(ap
   rep(mean(bp.xts[,2][bp.xts$High > 95]),len),
   rep(last(as.vector(na.omit(filter(apply.daily(bp.bangkok,mean)[,1],rep(1,7))/7))),len),
   rep(last(as.vector(na.omit(filter(apply.daily(bp.bangkok,mean)[,2],rep(1,7))/7))),len)),on=1,col=10,lwd=1)
-events <- xts(c("natrix","weight","abort natrix","70k","75k","80k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15")))
+# events <- xts(c("natrix","weight","abort natrix","70k","75k","80k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15")))
 addEventLines(events, srt=90, pos=2,col=10)
 axis(2,at=c(135,130,125,120,115,85,80,75,70))
 axis(4,at=c(135,130,125,120,115,85,80,75,70))
@@ -53,8 +53,9 @@ d <- intersect(intersect(index(bp.day["2019"]),seq(as.Date("2019-01-01"),as.Date
 
 # bp.day[seq(as.Date("2019-01-01"),as.Date("2019-12-31"),by='days')[d]]
 # bp.day[seq(as.Date("2018-01-01"),as.Date("2018-12-31"),by='days')[d]]
-# 
+#
 # merge(bp.day[seq(as.Date("2019-01-01"),as.Date("2019-12-31"),by='days')[d]],as.vector(bp.day[,1][seq(as.Date("2018-01-01"),as.Date("2018-12-31"),by='days')[d]]),as.vector(bp.day[,2][seq(as.Date("2018-01-01"),as.Date("2018-12-31"),by='days')[d]]),suffixes = c("","h18","h18"))
 
 
 plot(merge(bp.day[seq(as.Date("2019-01-01"),as.Date("2019-12-31"),by='days')[d]],as.vector(bp.day[,1][seq(as.Date("2018-01-01"),as.Date("2018-12-31"),by='days')[d]]),as.vector(bp.day[,2][seq(as.Date("2018-01-01"),as.Date("2018-12-31"),by='days')[d]]),suffixes = c("","h18","h18")))
+# addEventLines(events, srt=90, pos=2,col=10)
