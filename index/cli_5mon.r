db5fa9aa62b5b1e2e874919527cceed5cf7de868
onomoto <onomoto@me.com>
2019-04-21 16:37:29 +0900

    fix bug to draw graph w/ null color.

--- index/cli_5mon.r
+++ index/cli_5mon.r
@@ -4,24 +4,34 @@
 # lag is set to  5 months.
 #
 #   s = start date of the spiral like "1992-01-01::" DON'T FORGET DOUBLE COLON!!
-#   l = length of years like 9. use equal or less than 10.
+#   l = length of years like 9. use equal or less than 9
 #
 func <- function(s="1992-01-01::",l=9){
 
-  head_of_record <- "1962-01-01::"
+  head_of_record <- "1964-01-01::"
   print(head_of_record)
   offset <- length(seq(as.Date(head_of_record),as.Date(s),by='months'))
-  len_mon <- l*12
-  plot.default(na.trim(diff(cli_xts$oecd[head_of_record],lag=5))[offset:(offset+len_mon)],cli_xts$oecd[head_of_record][offset:(offset+len_mon)],type='b')
+  len_mon <- l*12-1
+  # len_mon <- l
+  print(offset)
+  print(len_mon)
+  print(seq(as.Date(head_of_record),as.Date(s),by='months')[offset])
+  print(seq(as.Date(head_of_record),as.Date("2100-01-01"),by='months')[offset+len_mon])
+
+  print(offset)
+  plot.default(na.trim(diff(cli_xts$oecd,lag=5))[head_of_record][offset:(offset+len_mon)],cli_xts$oecd[head_of_record][offset:(offset+len_mon)],type='b')
+  # print(offset)
   tmp <- par('usr')
   # par(new=T)
-  plot.default(na.trim(diff(cli_xts$oecd[head_of_record],lag=5))[offset:(offset+len_mon)],cli_xts$oecd[head_of_record][offset:(offset+len_mon)],type='b',xlim=c( tmp[1],tmp[2]), ylim=c(tmp[3], tmp[4]),col=1,lwd=1)
+  plot.default(na.trim(diff(cli_xts$oecd,lag=5))[head_of_record][offset:(offset+len_mon)],cli_xts$oecd[head_of_record][offset:(offset+len_mon)],type='b',xlim=c( tmp[1],tmp[2]), ylim=c(tmp[3], tmp[4]),lwd=1)
+  par(new=T)
   for(i in seq(0,l-1,1)){
     print(i)
-    print(offset)
+    # print(offset)
     print(offset+i*12)
-    print(offset+i*12+12)
-    plot.default(na.trim(diff(cli_xts$oecd[head_of_record],lag=5))[(offset+i*12):(offset+i*12+12)],cli_xts$oecd[head_of_record][(offset+i*12):(offset+i*12+12)],type='b',xlim=c( tmp[1],tmp[2]), ylim=c(tmp[3], tmp[4]),col=i,lwd=2)
+    print(offset+i*12+11)
+    par(new=T)
+    plot.default(na.trim(diff(cli_xts$oecd,lag=5))[head_of_record][(offset+i*12):(offset+i*12+11)],cli_xts$oecd[head_of_record][(offset+i*12):(offset+i*12+11)],type='b',xlim=c( tmp[1],tmp[2]), ylim=c(tmp[3], tmp[4]),col=i+1,lwd=2)
     par(new=T)
   }
   abline(v=0)
