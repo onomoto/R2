145370942e16c6f0100d569f682f28e25953dd51
onomoto <onomoto@me.com>
2019-04-24 12:07:14 +0900

    add parameter lag_month. change to use diff() to calculate delta. add total_terms variable.

--- index/vix_cli_sp.r
+++ index/vix_cli_sp.r
@@ -5,8 +5,12 @@
 #
 #
 #
-mnt <- index(cli_xts$oecd["2000::2019"][cli_xts$oecd["2000::2019"]/as.vector(cli_xts$oecd["1999-07-01::2018-07-01"]) < 1])
-plot.zoo(merge(VIX["2000::2019-01"][,4],VIX[mnt][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+lag_month <- 5
+# mnt <- index(cli_xts$oecd[total_terms][cli_xts$oecd[total_terms]/as.vector(cli_xts$oecd["1999-07-01::2018-07-01"]) < 1])
+total_terms <- paste("2000",index(last(cli_xts$oecd)),sep="::")
+
+mnt <- index(cli_xts$oecd[total_terms][na.omit(diff(cli_xts$oecd,lag=lag_month))[total_terms] < 0])
+plot.zoo(merge(VIX[total_terms][,4],VIX[mnt][,4]),type='h',col = c("red", "blue"), plot.type = "single")
 abline(v=seq(as.Date("2001-01-01"),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
 par(new=T)
-plot.default(SP5[,4]["2000::2019-01"],axes=F,type='l')
+plot.default(SP5[,4][total_terms],axes=F,type='l')
