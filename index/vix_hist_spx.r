4ab2f9039f4c35336b3206266d0b8fa41ee6d178
onomoto <onomoto@me.com>
2019-05-04 10:29:39 +0900

    cont'd the previous 1)improve handling start_date 2)calculate start of v-abline from the variable.

--- index/vix_hist_spx.r
+++ index/vix_hist_spx.r
@@ -7,17 +7,19 @@ if(file.exists("~/VIX.csv")){
 }else{
   print("!!!FILE VIX.csv DOESN'T EXIST!!!!!")
 }
-start_date <- "1990::"
+start_date <- "1995-01-01"
+# paste(start_date,"::",sep="")
 lag_month <- 5
-period <- paste(start_date,substr(as.character(index(last(cli_xts))),1,7),sep="")
+period <- paste(start_date,substr(as.character(index(last(cli_xts))),1,7),sep="::")
+
 # mnt <- index(cli_xts$oecd["2000::2019"][cli_xts$oecd["2000::2019"]/as.vector(cli_xts$oecd["1999-07-01::2018-08-01"]) < 1])
 # DON'T USE getsymbols as it has just limted scope of data.
 # getSymbols('^VIX',src="yahoo",auto.assign=TRUE)
 # VIX <- as.xts(read.zoo(read.csv("~/VIX.csv")))
-mnt4 <- index(cli_xts$oecd[start_date][na.omit(diff(cli_xts$oecd,lag=lag_month))[period] < 0 & na.omit(diff(cli_xts$oecd))[period] < 0])
+mnt4 <- index(cli_xts$oecd[period][na.omit(diff(cli_xts$oecd,lag=lag_month))[period] < 0 & na.omit(diff(cli_xts$oecd))[period] < 0])
 # plot.zoo(merge(VIX["2000::2019-02"][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
-plot.zoo(merge(VIX[paste(start_date,substr(as.character(index(last(cli_xts))),1,7),sep="")][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
-abline(v=seq(as.Date("2001-01-01"),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
+plot.zoo(merge(VIX[period][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+abline(v=seq(as.Date(start_date),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
 par(new=T)
 # plot.default(SP5[,4]["2000::2019-02"],axes=F,type='l')
 plot.default(SP5[,4][period],axes=F,type='l')
