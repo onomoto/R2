c4ba56b37363c1d14a733398db02d0af244ab568
onomoto <onomoto@me.com>
2019-05-07 20:20:42 +0900

    1) treat new weekly SP5. add "SP5 <- to.monthly(SP5)". 2) fuctionalize.

--- index/cli_delta_vs_gspc.r
+++ index/cli_delta_vs_gspc.r
@@ -1,11 +1,15 @@
 #
 # plot default spx 6month delta vs. composite leading indicator delta.
 #
-plot.default(diff(cli_xts$oecd,lag=6)["2007::2019-01"],diff(SP5[,4],lag=6)["2007::2019-01"])
-# abline(v=-0.81766,lty=2)
-abline(h=as.vector(last(diff(SP5[,4],lag=6)["2007::2019-01"])) ,lty=2)
-# abline(h=-112.1899,lty=2)
-abline(v=as.vector(last(diff(cli_xts$oecd,lag=6)["2007::2019-01"])),lty=2)
-abline(lm(diff(SP5[,4],lag=6)["2007::2019-01"] ~ diff(cli_xts$oecd,lag=6)["2007::2019-01"]))
-abline(v=0)
-abline(h=0)
+func <- function(){
+  SP5 <- to.monthly(SP5)
+  plot.default(diff(cli_xts$oecd,lag=6)["2007::2019-01"],diff(SP5[,4],lag=6)["2007::2019-01"])
+  # abline(v=-0.81766,lty=2)
+  abline(h=as.vector(last(diff(SP5[,4],lag=6)["2007::2019-01"])) ,lty=2)
+  # abline(h=-112.1899,lty=2)
+  abline(v=as.vector(last(diff(cli_xts$oecd,lag=6)["2007::2019-01"])),lty=2)
+  abline(lm(diff(SP5[,4],lag=6)["2007::2019-01"] ~ diff(cli_xts$oecd,lag=6)["2007::2019-01"]))
+  abline(v=0)
+  abline(h=0)
+}
+func()
