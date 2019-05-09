c055050d30ec3c6585ee485f183b088e94f75da0
onom-gtk-home <onomoto@me.com>
2019-05-09 22:36:25 +0900

    save image file w/ time stamp
    1)change to save the parent of the working directory. 2) adjust variable 'len' to fit path at the environment.

---
+++ index/Last.r
@@ -0,0 +1,17 @@
+
+.Last <- function() {
+  # save.image(file=paste(getwd(),Sys.time(),sep="/"))
+  #
+  # save working image in the parent directory. expected the length of the directory name is fixed.
+  # adjust value "len" to fit the length of the result "getwd()".
+  #
+  start <- 1
+  len <- 23
+  save.image(file=gsub(" ","-",paste(substr(getwd(),start,len),Sys.time(),sep="/")))
+  # 
+  # OR store working image at the working directory
+  #
+  # save.image(file=gsub(" ","-",paste(getwd(),Sys.time(),sep="/")))
+  #
+  cat("bye bye...\n")
+}
