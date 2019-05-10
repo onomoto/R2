07ddd85bcaebb74d46287811bc9728dc4ff45867
onom-gtk-office <onomoto@me.com>
2019-05-10 14:16:55 +0900

    VIX histgram color by CLI delta w/ SPX move.
    variable period length

---
+++ index/cli_delta_vs_spx_monthreturn.r
@@ -0,0 +1,66 @@
+#
+# pick up months whose monthlyreturn is less than 0.05 and put them to events
+#
+# argument 'p' could be either "2000-01-01::" or "2000-01-01::2018-12-31"
+# argument c is between 0 and 1
+# argument l is between 1 and 12
+#
+func <- function(p="2000-01-01",c=0.95,l=5){
+    if(nchar(p) > 12){
+        period <- p
+    } else{
+        period <- paste(p,index(last(cli_xts)),sep="::")
+    }
+    print(period)
+    lag_month <- l
+    change_rate <- c
+  # events <- xts(round(monthlyReturn(GSPC[start_date])[monthlyReturn(GSPC[start_date]) < -0.05],digits = 3),as.Date(mondate(index(monthlyReturn(GSPC[start_date])[monthlyReturn(GSPC[start_date]) < -0.05]))))
+    SP5 <- to.monthly(SP5)
+    # events <- xts(round(SP5[start_date][,4]/SP5[start_date][,1][SP5[start_date][,4]/SP5[start_date][,1] < change_rate],digits =4),as.Date(mondate(index(SP5[start_date][,4]/SP5[start_date][,1][SP5[start_date][,4]/SP5[start_date][,1] < change_rate]))))
+
+    event_date <- as.Date(mondate(index(SP5[period][,4]/SP5[period][,1][SP5[period][,4]/SP5[period][,1] < change_rate])))
+    mnt_bm <- index(cli_xts$oecd[period][na.omit(diff(cli_xts$oecd,lag=lag_month))[period] < 0 & na.omit(diff(cli_xts$oecd))[period] < 0])
+    mnt_mp <- index(cli_xts$oecd[period][na.omit(diff(cli_xts$oecd,lag=lag_month))[period] < 0 & na.omit(diff(cli_xts$oecd))[period] > 0])
+
+    plot.zoo(merge(VIX[period][,4],VIX[mnt_bm][,4],VIX[mnt_mp][,4]),type='h',col = c("blue", "red",rgb(1,0,1,alpha=0.9)), plot.type = "single",lwd=3)
+    if(nchar(p) > 12){
+        y_start <- seq(as.Date(substr(period,1,10)),as.Date(paste(substr(period,13,16),"-01-01",sep="")),by='years')
+    } else{
+        y_start <- seq(as.Date(substr(period,1,10)),as.Date("2019-01-01"),by='years')
+    }
+    for(i in seq(1,length(y_start),1)){
+        abline(v=y_start[i],col=rgb(0,1,0,alpha=0.9),lty=3)
+        print(i)
+    }
+    abline(v=y_start[1],col=rgb(0,1,0,alpha=0.9),lty=3)
+
+    # abline(v=seq(as.Date(start_date),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
+    # for(i in length(seq(as.Date(start_date),as.Date("2019-01-01"),by='years')),1)){
+    #     abline(v=seq(as.Date(start_date),as.Date("2019-01-01"),by='years')[i],col = "gray60",lty=3)
+    #     print(i)
+    # }
+    # abline(v=event_date[1],col = "gray60",lty=3)
+    for(i in seq(1,length(event_date),1)){
+        abline(v=event_date[i],col = "gray60",lty=2,lwd=2)
+        # print(i)
+    }
+    abline(v=event_date[1],col = "gray60",lty=2,lwd=2)
+  #
+  # draw graph of cli 5 months delta of oecd all, usa and china.
+  #
+  # addEventLines(events, srt=90, pos=2,col=10)  # this causes misalinghment
+  # addEventLines(events[c(6,7)], srt=90, pos=2,col=10)
+  #
+  # somehow "addEventLines(events, srt=90, pos=2,col=10)" works well
+  # vertical lines are put at wrong places. instead of it, put loop.
+  #
+#   for(i in seq(1,length(events),1)){
+#     addEventLines(events[i], srt=90, pos=2,col=4)
+#   }
+#   events
+#   #
+#   # and only the first entry to place.
+#   #
+#   addEventLines(events[1], srt=90, pos=2,col=4)
+}
+func("2000-01-01",0.95,5)
