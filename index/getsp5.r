e3e0df282f8a88a538f9eb7c52c29032cb30d37e
onom-gtk-home <onomoto@me.com>
2019-05-10 23:48:49 +0900

    improve weekday handling check file existence before read
    1)now recognize sunday, monday and other day.
    2)check file exists before read to avoid destroying data

--- index/getsp5.r
+++ index/getsp5.r
@@ -1,34 +1,55 @@
 func <- function(){
   # csv file below should be daily basis. otherwise it may have a wrong alingment after merged w/ GSPC.
   #
-  work <- as.xts(read.zoo(read.csv("~/git/R/index/CSV/SP5.csv")))
-  # getSymbols("^GSPC",auto.assign=TRUE)
-  ## testcode to handle weekdays and locale starts here.
+  # work <- as.xts(read.zoo(read.csv("~/git/R/index/CSV/SP5.csv")))
+  if(file.exists("~/git/R/index/CSV/SP5.csv")){
+    work <- as.xts(read.zoo(read.csv("~/git/R/index/CSV/SP5.csv")))
+  }else{
+    print("!!!FILE SP5.csv DOESN'T EXIST!!!!!")
+  }
+
   Sys.setlocale("LC_ALL",'en_US')
-  if(weekdays(Sys.Date()) != "Monday"){ 
-    if(as.Date(last(index(GSPC)))+1 != Sys.Date()){
+  today <- Sys.Date()
+  # today <- as.Date("2019-05-10")
+  wd <- weekdays(today)
+  # wd <- weekdays(as.Date("2019-05-10"))
+  last_update <- last(index(GSPC))
+  # last_update <- "2019-05-08"
+
+  if(wd == "Monday"){ 
+    if(as.Date(last_update)+3 != today){
       getSymbols("^GSPC",auto.assign=TRUE)
-      print("not monday")
+      print("monday  lazy friday")
       cat("the last update was")
-      print(as.Date(last(index(GSPC))))
+      print(as.Date(last_update))
     }else{
-      print("not monday but updated!")
+      print("monday and working friday!")
+    }
+  }else if(wd == "Sunday"){
+    if(as.Date(last_update)+2 != today){
+      getSymbols("^GSPC",auto.assign=TRUE)
+      print("sunday  lazy friday")
+      cat("the last update was")
+      print(as.Date(last_update))
+    }else{
+      print("sunday and working friday!")
     }
   }else{
-        if(as.Date(last(index(GSPC)))+3 != Sys.Date()){
-            getSymbols("^GSPC",auto.assign=TRUE)
-            print("monday  lazy friday")
-            cat("the last update was")
-            print(as.Date(last(index(GSPC))))
-        }else{
-        print("monday and friday!")
-        }
+    if(as.Date(last_update)+1 != today){
+      getSymbols("^GSPC",auto.assign=TRUE)
+      print("not monday, lazy yesterday")
+      cat("the last update was")
+      print(as.Date(last_update))
+    }else{
+      print("not monday, worked yesterday")
+    }
   }
   Sys.setlocale("LC_ALL",'ja_JP')
-  ## testcode ends here.
-  # coverting "GSPC" here is a bad idea. brought wrong alingment between years/weeks/months.
+
+  # coverting "GSPC" here is a bad idea. caused wrong alingment between years/weeks/months.
   # SP5 <<- append(work["::2006"],merge(to.weekly(GSPC)[,c(1,2,3,4,6)],to.weekly(GSPC)[,5]))
-  # use daily.
+  # append daily to daily to generate whole record.
+  # 
   SP5 <<- append(work["::2006"],merge(GSPC[,c(1,2,3,4,6)],GSPC[,5]))
   
 }
