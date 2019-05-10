92244c86529c20186e17b09d459bdc7278c4a5f0
onom-gtk-office <onomoto@me.com>
2019-05-10 14:20:30 +0900

    change name of parameter substr()
    substr() is given parameter of start position and last position, NOT start position and length. !!CAUTION!!

--- index/Last.r
+++ index/Last.r
@@ -5,11 +5,11 @@
   # save working image in the parent directory. expected the length of the directory name is fixed.
   # adjust value "len" to fit the length of the result "getwd()".
   #
-  start <- 1
+  start_pos <- 1
   # len <- 23
   # change to decide based upon strings length.
-  len <- nchar(as.character(getwd())) -2
-  save.image(file=gsub(" ","-",paste(substr(getwd(),start,len),Sys.time(),sep="/")))
+  last_pos <- nchar(as.character(getwd())) -2
+  save.image(file=gsub(" ","-",paste(substr(getwd(),start_pos,last_pos),Sys.time(),sep="/")))
   # 
   # OR store working image at the working directory
   #
