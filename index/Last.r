b0d68b492ebbb640ffa83d6e211d05f055267a15
onom-gtk-office <onomoto@me.com>
2019-05-10 10:44:55 +0900

    change to use nchar() to evaluate the length of pathname.

--- index/Last.r
+++ index/Last.r
@@ -6,7 +6,9 @@
   # adjust value "len" to fit the length of the result "getwd()".
   #
   start <- 1
-  len <- 23
+  # len <- 23
+  # change to decide based upon strings length.
+  len <- nchar(as.character(getwd())) -2
   save.image(file=gsub(" ","-",paste(substr(getwd(),start,len),Sys.time(),sep="/")))
   # 
   # OR store working image at the working directory
