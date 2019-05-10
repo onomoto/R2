7f01eb423b423a86042bd309ee8f956c72d5e8ad
onom-gtk-home <onomoto@me.com>
2019-05-11 00:06:48 +0900

    move beep(2) at the end

--- index/nikkei_gspc_jpy.r
+++ index/nikkei_gspc_jpy.r
@@ -87,9 +87,9 @@ N225 <- NIKKEI225
 result_nikkei <- lm(to.monthly(N225[k3])[,4] ~  to.monthly(GSPC[k3])[,4] + to.monthly(YJUSDJPY[k3])[,4])
 result_nikkei$coefficients[2]*last(GSPC)[,4]+result_nikkei$coefficients[3]*as.vector(last(YJUSDJPY)[,4])+result_nikkei$coefficients[1]
 # plot(merge(as.xts(predict(result_nikkei),index(residuals(result_nikkei))),to.monthly(N225[k3])[,4],residuals(result_nikkei)))
-beep(2)
+
 plot(merge(as.xts(predict(result_nikkei),index(residuals(result_nikkei))),to.monthly(N225[k3])[,4],residuals(result_nikkei)),main=paste(paste("NIKKEI225 =",round(result_nikkei$coefficients[2],4),"* GSPC +",round(result_nikkei$coefficients[3],2),"*USDJPY +",round(result_nikkei$coefficients[1],2))))
-
 tmp.legend <- paste("R Squared is ",round(summary(result_nikkei)$r.squared,4)," \n","DF is ",round(summary(result_nikkei)$df[2],0),sep=' ')
-
 addLegend(legend.loc = "topleft", legend.names = tmp.legend,col=3)
+
+beep(2)
