c24ccfe79908bf1c1b325fa754a182880efa071d
onom-gtk-home <onomoto@me.com>
2019-05-11 15:17:08 +0900

    change to use my_update_check to avoid unnecessary download
    N225,GSPC and YJUSDJPY

--- index/nikkei_gspc_jpy.r
+++ index/nikkei_gspc_jpy.r
@@ -2,86 +2,38 @@
 # https://00819.blogspot.com/2018/02/calculate-nikkei225-vol3.html
 #
 
-## test code to handle weekdays starts
-Sys.setlocale("LC_ALL",'en_US')
-today <- Sys.Date()
-# today <- as.Date("2019-05-10")
-wd <- weekdays(today)
-# wd <- weekdays(as.Date("2019-05-10"))
-last_update <- last(index(GSPC))
-# last_update <- "2019-05-08"
 
-if(wd == "Monday"){ 
-  if(as.Date(last_update)+3 != today){
-    getSymbols("^GSPC",auto.assign=TRUE)
-    print("monday  lazy friday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("monday and working friday!")
-  }
-}else if(wd == "Sunday"){
-  if(as.Date(last_update)+2 != today){
-    getSymbols("^GSPC",auto.assign=TRUE)
-    print("sunday  lazy friday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("sunday and working friday!")
-  }
-}else{
-  if(as.Date(last_update)+1 != today){
-    getSymbols("^GSPC",auto.assign=TRUE)
-    print("not monday, lazy yesterday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("not monday, worked yesterday")
-  }
+
+if(my_update_check(GSPC,Sys.Date()) == "F"){
+  print("SKIP")
+}
+if(my_update_check(GSPC,Sys.Date()) == "T"){
+  print("DOWNLOAD")
+  getSymbols("^GSPC",auto.assign=TRUE)
 }
 
-
-last_update <- last(index(YJUSDJPY))
-# last_update <- "2019-05-08"
-
-if(wd == "Monday"){ 
-  if(as.Date(last_update)+3 != today){
-    getSymbols("YJUSDJPY",src="yahooj")
-    print("monday  lazy friday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("monday and working friday!")
-  }
-}else if(wd == "Sunday"){
-  if(as.Date(last_update)+2 != today){
-    getSymbols("YJUSDJPY",src="yahooj")
-    print("sunday  lazy friday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("sunday and working friday!")
-  }
-}else{
-  if(as.Date(last_update)+1 != today){
-    getSymbols("YJUSDJPY",src="yahooj")
-    print("not monday, lazy yesterday")
-    cat("the last update was")
-    print(as.Date(last_update))
-  }else{
-    print("not monday, worked yesterday")
-  }
+if(my_update_check(YJUSDJPY,Sys.Date()) == "F"){
+  print("SKIP")
+}
+if(my_update_check(GSPC,Sys.Date()) == "T"){
+  print("DOWNLOAD")
+  getSymbols("YJUSDJPY",auto.assign=TRUE)
 }
 
-Sys.setlocale("LC_ALL",'ja_JP')
-## test code to handle weekdays ends
+if(my_update_check(N225,Sys.Date()) == "F"){
+  print("SKIP")
+}
+if(my_update_check(GSPC,Sys.Date()) == "T"){
+  print("DOWNLOAD")
+  getSymbols("N225",auto.assign=TRUE)
+}
 
 
 
 k3 <- paste("2007-01-01", index(last(GSPC)),sep="::")
 k3
 # download other data
-getSymbols("NIKKEI225",src="FRED",auto.assign=TRUE) # download nikkei 225
+# getSymbols("NIKKEI225",src="FRED",auto.assign=TRUE) # download nikkei 225
 N225 <- NIKKEI225
 
 result_nikkei <- lm(to.monthly(N225[k3])[,4] ~  to.monthly(GSPC[k3])[,4] + to.monthly(YJUSDJPY[k3])[,4])
