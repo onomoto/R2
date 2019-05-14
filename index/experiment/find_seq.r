c8371f11a50917a60a197d9d966400ef698da265
onom-gtk-office <onomoto@me.com>
2019-05-14 17:43:21 +0900

    fix bug after the end of loop.

--- index/experiment/find_seq.r
+++ index/experiment/find_seq.r
@@ -1,9 +1,8 @@
 op <- options(digits.secs = 6)
-Sys.time()
+# Sys.time()
 a <- Sys.time()
 counter <- 1
-
-limit <- 4
+limit <- 6
 for( i in seq(2,length(index(SP5)),1)){
   if(SP5[,4][i] < as.vector(SP5[,4][i-1])){
     counter <- counter +1
@@ -17,8 +16,17 @@ for( i in seq(2,length(index(SP5)),1)){
       counter <- 0
   }
 }
-
-Sys.time()
+# cat(counter)
+# cat(" ends at ")
+# cat(as.character(index(SP5[,4][i-1])))
+# when loop ends check countr and output if necessary.
+if(counter >= limit){
+  cat(counter)
+  cat(" day at ")
+  cat(as.character(index(SP5[,4][i-1])))
+  cat("\n")
+}
+# Sys.time()
 b <- Sys.time()
 b - a
 op <- options(digits.secs = 2)
