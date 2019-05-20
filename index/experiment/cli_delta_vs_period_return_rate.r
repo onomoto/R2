5df6f1c877e68ad49b802f41a011d4bbf11e5ff1
onom-gtk-home <onomoto@me.com>
2019-05-20 19:53:01 +0900

    fix bug: variable "lag_month" is not applied to where should be
    change to make xts as result.

--- index/experiment/cli_delta_vs_period_return_rate.r
+++ index/experiment/cli_delta_vs_period_return_rate.r
@@ -11,44 +11,48 @@ start_index <- 1
 iteration <- 0
 performance_val <- c()
 period_length <- c()
-lag_month <- 5
+lag_month <- 1
+result <- c()
+rate <- c()
 
 
 for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
-  if(as.vector(diff(cli_xts$oecd)[period])[i] > 0){  # up is "> 0"
+  if(as.vector(diff(cli_xts$oecd,lag=lag_month)[period])[i] > 0){  # up is "> 0"
     w <- append(w,1)
   }else{
     w <- append(w,0)
   }
 }
 month_flag <- 0
-for(i in seq(1,length(diff(cli_xts$oecd)[period]),1,)){
+for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
   if(w[i] == 1){
     if(month_flag == 0){
       month_flag <- 1
       start_price <- as.vector(to.monthly(SP5[period])[,4][i])
       # print(index(to.monthly(SP5[period])[,4][i]))
-      cat("from ")
-      cat(as.character(as.Date(index(to.monthly(SP5[period])[,4][i]))))
+      # cat("from ")
+      # cat(as.character(as.Date(index(to.monthly(SP5[period])[,4][i]))))
       start_index <- i
     }
   }else if(w[i] ==0){
     if(month_flag == 1){
-      cat(" ")
-      cat(i - start_index)
-      cat(" month(s)")
-      cat("\n")
+      # cat(" ")
+      # cat(i - start_index)
+      # cat(" month(s)")
+      # cat("\n")
       iteration <- iteration +1
       print(iteration)
-      print(as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
+      result <- append(result,as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
+      # print(as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
       # print(i - start_index)
       month_flag <- 0
       period_length <- append(period_length,i-start_index)
       performance_val <- append(performance_val,as.vector(to.monthly(SP5[period])[,4][i]) / start_price)
-      print(last(performance_val)**(1/last(period_length)))
+      rate <- append(rate,last(performance_val)**(1/last(period_length))-1)
     }
   }
 }
+merge(result,period_length,rate)
 mean(period_length)
 mean(performance_val)
 # t_minus <- performance_val
