30987994aea7cd5cb54f5a196bf0e98375771b6e
onom-gtk-office <onomoto@me.com>
2019-05-21 10:26:54 +0900

    functionalize
    1)now return xts object as output. 
    2)new parameter to specify end date.

--- index/experiment/cli_delta_vs_period_return_rate.r
+++ index/experiment/cli_delta_vs_period_return_rate.r
@@ -1,59 +1,88 @@
 #
 # 1)pick up months whose clie_xts$oecd  is up from the previous
-# 2)crate the stream of flags, in which up is 1 and down is 0
-# 3)compare SPX close price between the end month of cli_xts delta is plus and its start.
-#
-w <- c()
-last_date <- last(index(cli_xts$oecd))
-start_date <- "1970-01-01"
-period <- paste(start_date,last_date,sep='::')
-start_index <- 1
-iteration <- 0
-performance_val <- c()
-period_length <- c()
-lag_month <- 1
-result <- c()
-rate <- c()
+# 2)create the stream of flags, in which up is 1 and down is 0
+# 3)compare SPX close price between the end month of cli_xts delta is plus(or minus) and its start.
+# 4)return xts objects which contains. start month of period, updown ration, length of months and monthly average return during period.
 
-
-for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
-  if(as.vector(diff(cli_xts$oecd,lag=lag_month)[period])[i] > 0){  # up is "> 0"
-    w <- append(w,1)
+func <- function(pm="plus",s="1970-01-01",l=1){
+  w <- c()
+  cat("0")
+  cat(length(s))
+  if(nchar(s) == 10){ # use nchar() to measure strings length, not length()
+    cat("1")
+    last_date <- last(index(cli_xts$oecd))
+    start_date <- s
+    period <- paste(start_date,last_date,sep='::')
   }else{
-    w <- append(w,0)
+     # last_date <- l
+     period <- s
   }
-}
-month_flag <- 0
-for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
-  if(w[i] == 1){
-    if(month_flag == 0){
-      month_flag <- 1
-      start_price <- as.vector(to.monthly(SP5[period])[,4][i])
-      # print(index(to.monthly(SP5[period])[,4][i]))
-      # cat("from ")
-      # cat(as.character(as.Date(index(to.monthly(SP5[period])[,4][i]))))
-      start_index <- i
-    }
-  }else if(w[i] ==0){
-    if(month_flag == 1){
-      # cat(" ")
-      # cat(i - start_index)
-      # cat(" month(s)")
-      # cat("\n")
-      iteration <- iteration +1
-      print(iteration)
-      result <- append(result,as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
-      # print(as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
-      # print(i - start_index)
-      month_flag <- 0
-      period_length <- append(period_length,i-start_index)
-      performance_val <- append(performance_val,as.vector(to.monthly(SP5[period])[,4][i]) / start_price)
-      rate <- append(rate,last(performance_val)**(1/last(period_length))-1)
+  # last_date <- last(index(cli_xts$oecd))
+  # start_date <- s
+  # period <- paste(start_date,last_date,sep='::')
+  start_index <- 1
+  iteration <- 0
+  performance_val <- c()
+  period_length <- c()
+  lag_month <- l
+  result <- c()
+  rate <- c()
+  plus_or_minus <- pm
+
+# put flag on the months accoring to the parameter. for "minus" cli delta is less than ZERO, for plus the opposite.
+  for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
+    if(plus_or_minus == "minus"){
+      if(as.vector(diff(cli_xts$oecd,lag=lag_month)[period])[i] < 0){  # up is "> 0"
+        w <- append(w,1)
+      }else{
+        w <- append(w,0)
+      }
+    }else if(plus_or_minus == "plus"){
+      if(as.vector(diff(cli_xts$oecd,lag=lag_month)[period])[i] > 0){  # up is "> 0"
+        w <- append(w,1)
+      }else{
+        w <- append(w,0)
+      }
     }
   }
+  month_flag <- 0 # status flag
+# check stream and when flag is changes 0 to 1. it is the start of period.
+  for(i in seq(1,length(diff(cli_xts$oecd,lag=lag_month)[period]),1,)){
+    if(w[i] == 1){
+      if(month_flag == 0){ # when w is 1 and month_flag is 0, the period starts
+        month_flag <- 1
+        start_price <- as.vector(to.monthly(SP5[period])[,4][i])
+        # print(index(to.monthly(SP5[period])[,4][i]))
+        # cat("from ")
+        # cat(as.character(as.Date(index(to.monthly(SP5[period])[,4][i]))))
+        start_index <- i
+      }
+# check stream and when flag is changes 1 to 0. it is the start of period.
+    }else if(w[i] ==0){
+      if(month_flag == 1){ # when w is 0 and month_flag is 1, the period ends
+        # cat(" ")
+        # cat(i - start_index)
+        # cat(" month(s)")
+        # cat("\n")
+        iteration <- iteration +1
+        cat(iteration)
+        cat(" ")
+        result <- append(result,as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
+        # print(as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
+        # print(i - start_index)
+        month_flag <- 0 # when period ends, intialize the flag.
+        period_length <- append(period_length,i-start_index)
+        performance_val <- append(performance_val,as.vector(to.monthly(SP5[period])[,4][i]) / start_price)
+        rate <- append(rate,last(performance_val)**(1/last(period_length))-1)
+      }
+    }
+  }
+  cat("\n")
+  print(mean(period_length))
+  print(mean(performance_val))
+  return(merge(result,period_length,rate))
+
 }
-merge(result,period_length,rate)
-mean(period_length)
-mean(performance_val)
 # t_minus <- performance_val
-t_plus <- performance_val
+# t_plus <- performance_val
+func("plus","1970-01-01")
