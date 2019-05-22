dee521ed9a48800be6cd23733c64ffbdf6de402b
onom-gtk-office <onomoto@me.com>
2019-05-22 11:13:06 +0900

    dc: now output to xts objects 'Events'

--- index/experiment/find_seq.r
+++ index/experiment/find_seq.r
@@ -3,15 +3,17 @@ op <- options(digits.secs = 6)
 a <- Sys.time()
 counter <- 1
 limit <- 6
+Events <- c()
 for( i in seq(2,length(index(SP5)),1)){
   if(SP5[,4][i] < as.vector(SP5[,4][i-1])){
     counter <- counter +1
   }else{
       if(counter >= limit){
-        cat(counter)
-        cat(" day at ")
-        cat(as.character(index(SP5[,4][i-1])))
-        cat("\n")
+        # cat(counter)
+        # cat(" day at ")
+        # cat(as.character(index(SP5[,4][i-1])))
+        # cat("\n")
+        Events <<- append(Events,as.xts(counter,index(SP5[,4][i-1])))
       }
       counter <- 0
   }
@@ -21,12 +23,14 @@ for( i in seq(2,length(index(SP5)),1)){
 # cat(as.character(index(SP5[,4][i-1])))
 # when loop ends check countr and output if necessary.
 if(counter >= limit){
-  cat(counter)
-  cat(" day at ")
-  cat(as.character(index(SP5[,4][i-1])))
-  cat("\n")
+  # cat(counter)
+  # cat(" day at ")
+  # cat(as.character(index(SP5[,4][i-1])))
+  # cat("\n")
+  Events <<- append(Events,as.xts(counter,index(SP5[,4][i-1])))
 }
 # Sys.time()
+Events
 b <- Sys.time()
 b - a
 op <- options(digits.secs = 2)
