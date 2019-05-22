17cf4c39c5b53e5017397981288639130dd9c0f2
onom-gtk-home <onomoto@me.com>
2019-05-22 20:21:29 +0900

    wip: rewrite code from design.

--- index/experiment/find_correction.r
+++ index/experiment/find_correction.r
@@ -4,71 +4,46 @@ a <- Sys.time()
 j <- 0
 correction_ratio <- 0.9
 hist_high <- c()
-recent_high <- c() 
-correction_flag <- 0
+recent_high <- c()
+correction_flag <- c()
+status_flag <- 0
 depth <- c()
 # depth <- as.xts(0,0,0,as.Date("1950-01-01"))
 depth_ratio <- 0
 depth_new <- c()
 depth_date <- as.Date("1950-01-01")
-for( i in seq(2,length(index(SP5)),1)){
-  if(SP5[,4][i] > as.vector(SP5[,4][i-1])){
-    if(SP5[,4][i] > hist_high){
-      hist_high <- hist_high
+search_range <- 250
+for( i in seq(search_range,length(index(SP5)),1)) {
+  z <- i-search_range+1
+  recent_high <- append(recent_high,as.xts(max(SP5[,4][z:i]),index(SP5[i])))
+  if(SP5[,4][i] < correction_ratio * last(recent_high)[,1]){
+      correction_flag <- append(correction_flag,1)
+      # hist_high <- append(hist_high
       # c(hist_high,as.xts(hist_high,as.vector(SP5[,4][i]),index(SP5[,4][i])))
-    }
-    # if today's price is higher than the previous day
-    if(SP5[,4][i] > 1.1*as.vector(last(depth[,1]))) {
-      # check if it is higher than historical high
-      # if so update historical high record.
-      recent_high <- as.vector(SP5[,4][i])
-      correction_flag <- 0
-      if(depth_ratio !=1){
-        # cat("deepest is ")
-        # cat(round(depth*100,digits=2))
-        # cat(" % at the date of ")
-        # cat(as.character(depth_date))
-        print(last(depth))
-        cat("\n ######## \n")
-      }
-      depth_ratio <- 1
-      depth_new <- 1
-    }
-
   }else{
-    if(SP5[,4][i] < correction_ratio * recent_high){
-      depth_new <- as.vector(SP5[,4][i] / recent_high)
-      if(correction_flag == 0){
-
-        cat(round(100-correction_ratio*100,digits=2))
-        cat("% correction starts at ")
-        cat(as.character(index(SP5[,4][i])))
-        # print(as.Date(index(SP5[,4][i])))
-        cat(" \n")
-        correction_flag <- 1
-      }
-      # cat(depth)
-      # cat(depth_new)
-      if(depth_ratio >= depth_new){
-
-          depth_ratio <- depth_new
-          # depth_date <- index(SP5[,4][i])
-          # depth_close <- as.vector(SP5[,4][i])
-          if(length(index(depth)) == 1){
-            print("1")
-            depth <- as.xts(depth_new,as.vector(SP5[,4][i]),hist_high,index(SP5[,4][i]))
-            print("2")
-          }else{
-            print("3")
-            depth <- append(depth,as.xts(depth_new,as.vector(SP5[,4][i]),hist_high,index(SP5[,4][i])))  
-            print("4")
-          }
-
-          # depth <- append(depth,as.xts(depth_new,as.vector(SP5[,4][i]),hist_high,index(SP5[,4][i])))
-
-      }
-    }
+      correction_flag <- append(correction_flag,0)
   }
+  if(z > 17000){
+    cat(z)
+    cat(" ")
+  }
+  # cat(z)
+  # cat(" ")
+}
+recent_high <- merge(recent_high,correction_flag)
+recent_high <- merge(recent_high,SP5[,4][search_range:length(index(SP5))])
+for(i in seq(1,length(index(recent_high)),1)){
+  if(recent_high[,2][i] == 1 && status_flag == 0){
+    print("starts at ")
+    print(index(recent_high[i]))
+    status_flag <- 1
+  }
+  if(recent_high[,2][i] == 0 && status_flag == 1){
+    print("ends at ")
+    print(index(recent_high[i]))
+    status_flag <- 0
+  }
+
 }
 Sys.time()
 b <- Sys.time()
