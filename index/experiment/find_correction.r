f759308f2293390b664e1b08f3ac3ab58fa17692
onom-gtk-home <onomoto@me.com>
2019-05-26 19:18:27 +0900

    dc: add code to calculate day distance and append its result

--- index/experiment/find_correction.r
+++ index/experiment/find_correction.r
@@ -24,7 +24,7 @@ for( i in seq(search_range,length(index(SP5)),1)) {
       # c(hist_high,as.xts(hist_high,as.vector(SP5[,4][i]),index(SP5[,4][i])))
   }else{
     # exit criateria is 5% above the one to enter.
-    if(SP5[,4][i] > (0.05+correction_ratio) * last(recent_high)[,1]){ 
+    if(SP5[,4][i] > (0.05+correction_ratio) * last(recent_high)[,1]){
       correction_flag <- append(correction_flag,0)
     }else{
       if(last(correction_flag) == 1){ # even if current price is less than exit criteria
@@ -33,7 +33,7 @@ for( i in seq(search_range,length(index(SP5)),1)) {
         correction_flag <- append(correction_flag,0) # if it is not already in correction. it is NOT correction.
       }
       # correction_flag <- append(correction_flag,1) #otherwise correction continues.
-    } 
+    }
   }
   if((z %% 500) == 0){
     cat(".")
@@ -45,6 +45,20 @@ for( i in seq(search_range,length(index(SP5)),1)) {
 recent_high <- merge(recent_high,correction_flag)
 recent_high <- merge(recent_high,SP5[,4][search_range:length(index(SP5))])
 for(i in seq(1,length(index(recent_high)),1)){
+  #
+  # in the middle of correction
+  # don't put this after "at the start of correction" section
+  if(recent_high[,2][i] == 1 && status_flag == 1){
+    # print("starts at ")
+    # print(index(recent_high[i]))
+    output <- append(output,
+    as.xts(recent_high[,3][i]/recent_high[,1][i],index(recent_high[i])))
+    if(depth_ratio > as.vector(recent_high[,3][i]/recent_high[,1][i])){
+      depth_ratio <- recent_high[,3][i]/recent_high[,1][i]
+    }
+    day_count <- day_count +1
+    # status_flag <- 1
+  }
   # at the start of correction
   if(recent_high[,2][i] == 1 && status_flag == 0){
     # print("starts at ")
@@ -57,18 +71,7 @@ for(i in seq(1,length(index(recent_high)),1)){
     status_flag <- 1
     day_count <- day_count +1
   }
-  # in the middle of correction
-  if(recent_high[,2][i] == 1 && status_flag == 1){
-    # print("starts at ")
-    # print(index(recent_high[i]))
-    output <- append(output,
-    as.xts(recent_high[,3][i]/recent_high[,1][i],index(recent_high[i])))
-    if(depth_ratio > as.vector(recent_high[,3][i]/recent_high[,1][i])){
-      depth_ratio <- recent_high[,3][i]/recent_high[,1][i]
-    }
-    day_count <- day_count +1
-    # status_flag <- 1
-  }
+
   # at the end of correction
   if(recent_high[,2][i] == 0 && status_flag == 1){
     # cat("start at ")
@@ -86,6 +89,21 @@ for(i in seq(1,length(index(recent_high)),1)){
     day_count <- 0
   }
 }
+# dc: add code to calculate day distance and append its result
+len <- c()
+for(i in seq(1,length(index(output)),1)) {
+  len <- append(len,length(index(SP5[paste(as.character(index(SP5[1])),as.character(index(output[i])),sep="::")])))
+  # cat("")
+  # cat(i)
+  # cat(i)
+  # cat(" ")
+  if((i %% 500) == 0){
+    cat(".")
+    cat("")
+  }
+}
+output <- merge(output,len)
+
 #
 # or plot.xts() graph
 #
