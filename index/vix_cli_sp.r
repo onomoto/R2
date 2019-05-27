62ace0e0dbd254c1bb03d1961d70c46a99b1260f
onom-gtk-office <onomoto@me.com>
2019-05-27 13:57:53 +0900

    dc: change color scheme.  cli delta minus = red. plus = blue.
    dc: change color scheme cli delta minus = red. plus = blue.
    
    
    dc: change color scheme

--- index/vix_cli_sp.r
+++ index/vix_cli_sp.r
@@ -10,7 +10,7 @@ lag_month <- 5
 total_terms <- paste("2000",index(last(cli_xts$oecd)),sep="::")
 
 mnt <- index(cli_xts$oecd[total_terms][na.omit(diff(cli_xts$oecd,lag=lag_month))[total_terms] < 0])
-plot.zoo(merge(VIX[total_terms][,4],VIX[mnt][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+plot.zoo(merge(VIX[total_terms][,4],VIX[mnt][,4]),type='h',col = c("blue", "red"), plot.type = "single")
 abline(v=seq(as.Date("2001-01-01"),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
 par(new=T)
 plot.default(SP5[,4][total_terms],axes=F,type='l')
--- index/vix_hist_spx.r
+++ index/vix_hist_spx.r
@@ -1,7 +1,7 @@
 # 
 # draw overlay graph of 
 # 1) VIX close histgram and 
-# 2)SPX line graph. the histgram is draw blue when both CLI 5 month and 1month delta is less than 0.
+# 2)SPX line graph. the histgram is draw red when both CLI 5 month and 1month delta is less than 0.
 # 
 if(file.exists("~/VIX.csv")){
   # VIX <- read.csv("~/VIX.csv")
@@ -17,7 +17,7 @@ period <- paste(start_date,substr(as.character(index(last(cli_xts))),1,10),sep="
 # mnt <- index(cli_xts$oecd["2000::2019"][cli_xts$oecd["2000::2019"]/as.vector(cli_xts$oecd["1999-07-01::2018-08-01"]) < 1])
 mnt4 <- index(cli_xts$oecd[period][na.omit(diff(cli_xts$oecd,lag=lag_month))[period] < 0 & na.omit(diff(cli_xts$oecd))[period] < 0])
 # plot.zoo(merge(VIX["2000::2019-02"][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
-plot.zoo(merge(VIX[period][,4],VIX[mnt4][,4]),type='h',col = c("red", "blue"), plot.type = "single")
+plot.zoo(merge(VIX[period][,4],VIX[mnt4][,4]),type='h',col = c("blue", "red"), plot.type = "single")
 abline(v=seq(as.Date(start_date),as.Date("2019-01-01"),by='years'), col=rgb(0,1,0,alpha=0.9),lty=2)
 par(new=T)
 # plot.default(SP5[,4]["2000::2019-02"],axes=F,type='l')
