5395ee4f15a301b7f9535cd9ac98943da9d6d862
onom-gtk-office <onomoto@me.com>
2019-06-05 10:17:49 +0900

    bf : 0605 have to put the end month of record at the end of loop.
    bf : 0605 truncate date section of last(index(cli_xts)) as it causes misalignment.

--- index/experiment/cli_delta_vs_period_return_rate.r
+++ index/experiment/cli_delta_vs_period_return_rate.r
@@ -10,7 +10,7 @@ func <- function(pm="plus",s="1970-01-01",l=1){
   cat(length(s))
   if(nchar(s) == 10){ # use nchar() to measure strings length, not length()
     cat("1")
-    last_date <- last(index(cli_xts$oecd))
+    last_date <- substr(last(index(cli_xts$oecd)),1,7)
     start_date <- s
     period <- paste(start_date,last_date,sep='::')
   }else{
@@ -63,12 +63,13 @@ func <- function(pm="plus",s="1970-01-01",l=1){
       }
       # dc 0602 add output at the end of loop
       if(i == length(diff(cli_xts$oecd,lag=lag_month)[period])){
-        # print("end of the loop")
+        print("end of the loop")
         result <- append(result,as.xts(as.vector(to.monthly(SP5[period])[,4][i]) / start_price,index(to.monthly(SP5[period])[,4][i])))
                 period_length <- append(period_length,i-start_index)
-        performance_val <- append(performance_val,as.vector(to.monthly(SP5[period])[,4][i-1]) / start_price)
+        performance_val <- append(performance_val,as.vector(to.monthly(SP5[period])[,4][i]) / start_price) # bf 0605 put the end month of record at the end of the loop.
         open_p <- append(open_p,start_price)
-        close_p <- append(close_p,as.vector(to.monthly(SP5[period])[,4][i-1]))
+        print(i)
+        close_p <- append(close_p,as.vector(to.monthly(SP5[period])[,4][i])) # bf 0605 same as above.
         rate <- append(rate,last(performance_val)**(1/last(period_length))-1)
       }
 # check stream and when flag is changes 1 to 0. it is the start of period.
