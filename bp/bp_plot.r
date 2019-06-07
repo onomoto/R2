15733369484bb0e171f5b880487bd05ef8a03938
onom-gtk-home <onomoto@me.com>
2019-06-08 06:15:10 +0900

    dc: adding '90k' in events.

--- bp/bp_plot.r
+++ bp/bp_plot.r
@@ -9,7 +9,8 @@ major.ticks='days',grid.ticks.on='days',type='p',ylim=c(60,160))
 addSeries(merge(as.xts(rep(125,len),index(bp.xts[start_date][bp.xts[start_date]$High > 95])),rep(75,len),
      rep(mean(bp.xts[start_date][,1][bp.xts[start_date]$High > 95]),len),
      rep(mean(bp.xts[start_date][,2][bp.xts[start_date]$High > 95]),len)), ylim=c(60,160),on=1,col=4,lwd=1)
-events <- xts(c("weight","70k","75k","80k","85k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+# events <- xts(c("weight","70k","75k","80k","85k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
 addEventLines(events, srt=90, pos=2,col=10)
 axis(4,at=c(seq(70,140,5)))
 
@@ -25,7 +26,8 @@ addSeries(merge(as.xts(rep(mean(bp.xts[,1][bp.xts$High > 95]),len),last(index(ap
   rep(mean(bp.xts[,2][bp.xts$High > 95]),len),
   rep(last(as.vector(na.omit(filter(apply.daily(bp.bangkok,mean)[,1],rep(1,7))/7))),len),
   rep(last(as.vector(na.omit(filter(apply.daily(bp.bangkok,mean)[,2],rep(1,7))/7))),len)),on=1,col=10,lwd=1)
-events <- xts(c("natrix","weight","abort natrix","70k","75k","80k","85k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+# events <- xts(c("natrix","weight","abort natrix","70k","75k","80k","85k"),as.Date(c("2018-06-20", "2018-07-14","2018-08-09","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
 addEventLines(events, srt=90, pos=2,col=10)
 axis(2,at=c(135,130,125,120,115,85,80,75,70))
 axis(4,at=c(135,130,125,120,115,85,80,75,70))
@@ -55,7 +57,7 @@ plot(apply.weekly(bp.day,mean),type='p',ylim=c( min(apply.weekly(bp.day,mean)[,2
 addSeries(as.xts(rep(last(apply.weekly(bp.day,mean)[,1]),length(apply.weekly(bp.day,mean)[,1])),index(apply.weekly(bp.day,mean))),on=1,ylim=c( min(apply.weekly(bp.day,mean)[,2])-10,max(apply.weekly(bp.day,mean)[,1])+10),col=2)
 addSeries(as.xts(rep(last(apply.weekly(bp.day,mean)[,2]),length(apply.weekly(bp.day,mean)[,1])),index(apply.weekly(bp.day,mean))),on=1,ylim=c( min(apply.weekly(bp.day,mean)[,2])-10,max(apply.weekly(bp.day,mean)[,1])+10),col=2)
 
-events <- xts(c("weight","70k","75k","80k","85k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
 addEventLines(events, srt=90, pos=2,col=10)
 axis(2,at=c(135,130,125,120,115,85,80,75,70))
 axis(4,at=c(135,130,125,120,115,85,80,75,70))
