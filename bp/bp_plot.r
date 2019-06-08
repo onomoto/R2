e1cd824c3c3302eecb0118528b6dc4264c3ed769
onom-gtk-home <onomoto@me.com>
2019-06-08 09:37:49 +0900

    bf: 0608 move addeventlines in the loop

--- bp/bp_plot.r
+++ bp/bp_plot.r
@@ -10,8 +10,13 @@ addSeries(merge(as.xts(rep(125,len),index(bp.xts[start_date][bp.xts[start_date]$
      rep(mean(bp.xts[start_date][,1][bp.xts[start_date]$High > 95]),len),
      rep(mean(bp.xts[start_date][,2][bp.xts[start_date]$High > 95]),len)), ylim=c(60,160),on=1,col=4,lwd=1)
 # events <- xts(c("weight","70k","75k","80k","85k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16")))
+# events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
+# addEventLines(events, srt=90, pos=2,col=10)
 events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
-addEventLines(events, srt=90, pos=2,col=10)
+for(i in seq(1,length(index(events)),1)){
+  addEventLines(events[i], srt=90, pos=2,col=10)
+}
+addEventLines(events[1], srt=90, pos=2,col=10)
 axis(4,at=c(seq(70,140,5)))
 
 #
@@ -58,6 +63,9 @@ addSeries(as.xts(rep(last(apply.weekly(bp.day,mean)[,1]),length(apply.weekly(bp.
 addSeries(as.xts(rep(last(apply.weekly(bp.day,mean)[,2]),length(apply.weekly(bp.day,mean)[,1])),index(apply.weekly(bp.day,mean))),on=1,ylim=c( min(apply.weekly(bp.day,mean)[,2])-10,max(apply.weekly(bp.day,mean)[,1])+10),col=2)
 
 events <- xts(c("weight","70k","75k","80k","85k","90k"),as.Date(c("2018-07-14","2019-01-23","2019-02-14","2019-03-15","2019-4-16","2019-06-07")))
-addEventLines(events, srt=90, pos=2,col=10)
+for(i in seq(1,length(index(events)),1)){
+  addEventLines(events[i], srt=90, pos=2,col=10)
+}
+addEventLines(events[1], srt=90, pos=2,col=10)
 axis(2,at=c(135,130,125,120,115,85,80,75,70))
 axis(4,at=c(135,130,125,120,115,85,80,75,70))
