a52e28b861a9db2d2ee0120f5a59371413128175
onom-gtk-home <onomoto@me.com>
2019-06-17 20:15:12 +0900

    bf : use getwd() to enable to work on other hosts.

--- index/01day.r
+++ index/01day.r
@@ -1,5 +1,8 @@
-source('~/R_proj/R/index/getsp5.r', echo=TRUE)
-source('~/R_proj/R/index/nikkei_gspc_jpy.r', echo=TRUE)
+# source('~/R_proj/R/index/getsp5.r', echo=TRUE)
+# source('~/R_proj/R/index/nikkei_gspc_jpy.r', echo=TRUE)
+
+source(paste(getwd(),"index/getsp5.r",sep='/'),echo=TRUE)
+source(paste(getwd(),"index/nikkei_gspc_jpy.r",sep='/'),echo=TRUE)
 if(index(tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)]) == index(last(to.monthly(SP5))[,c(1,2,3,4,5)])){ 
     tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)] <- last(to.monthly(SP5))[,c(1,2,3,4,5)]
 }else{
