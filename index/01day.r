52e061ae624532d7374e399c7d3dc15c785ad36d
onom-gtk-office <onomoto@me.com>
2019-06-17 15:12:24 +0900

    er : add daily update tmp.predict

--- index/01day.r
+++ index/01day.r
@@ -1,2 +1,7 @@
 source('~/R_proj/R/index/getsp5.r', echo=TRUE)
 source('~/R_proj/R/index/nikkei_gspc_jpy.r', echo=TRUE)
+if(index(tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)]) == index(last(to.monthly(SP5))[,c(1,2,3,4,5)])){ 
+    tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)] <- last(to.monthly(SP5))[,c(1,2,3,4,5)]
+}else{
+    print("A new month has come. Please update!!!")
+}
