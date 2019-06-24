9872d7fc4443e57907df9ef3ae1712e02954a45c
onom-gtk-office <onomoto@me.com>
2019-06-24 17:35:31 +0900

    dc : add ylim= for better presentation.

--- index/experiment/diff_pa.r
+++ index/experiment/diff_pa.r
@@ -6,7 +6,7 @@ func <- function(m=6){
     len_mon <- length(seq(as.Date("2012-01-01"),index(last(PA)),by='months'))
     mov_mon <- m
     w <- merge(last(na.omit(diff(PA)),n=len_mon),last(na.omit(filter(diff(PA),rep(1,mov_mon))/mov_mon),n=len_mon),last(na.omit(filter(diff(PA),rep(1,mov_mon*2))/(mov_mon*2)),n=len_mon))
-    plot(w[,1],type='h')
+    plot(w[,1],type='h',ylim=c(0,max(w[,1]*1.05)))
     lines(w[,2],col=4)
     lines(w[,3],col=2)
 }
