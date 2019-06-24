5b7d841c38998f26b6ac0ab6a7fac95eee85c9c7
onom-gtk-office <onomoto@me.com>
2019-06-24 17:28:22 +0900

    dc : change to use plot.xts() and lines() for reading and mov avg.

--- index/experiment/diff_pa.r
+++ index/experiment/diff_pa.r
@@ -1,11 +1,13 @@
 #
-# 1) draw graph PAEMS delta and its 6month moving average.
-# 2) length is fixed to 90 months from the current most updated data available month.
+# 1) draw graph PAEMS delta and its moving average. use 'mov_mon' to adjust it.
+# 2) start is fixed to 2012-01-01 and the end is the current most updated data available month.
 #
 len_mon <- length(seq(as.Date("2012-01-01"),index(last(PA)),by='months'))
 mov_mon <- 6
-plot(last(seq(as.Date("2001-01-01"),index(last(PA)),by='months'),n=len_mon),last(na.omit(filter(diff(PA),rep(1,1))/1),n=len_mon),type='h',axes=T,ylim=c(0,400),lwd=4)
-par(new=T)
-plot(last(seq(as.Date("2001-01-01"),Sys.Date(),by='months'),n=len_mon),last(na.omit(filter(diff(PA),rep(1,mov_mon))/mov_mon),n=len_mon),type='l',col=2,axes=F,ylim=c(0,400),lwd=2)
-abline(h=seq(80,340,20),lty=2,col=4)
-abline(v=seq(as.Date("2013-02-01"),as.Date("2019-02-01"),by='years'),lty=2,col=4)
+
+
+
+w <- merge(last(na.omit(diff(PA)),n=len_mon),last(na.omit(filter(diff(PA),rep(1,mov_mon))/mov_mon),n=len_mon),last(na.omit(filter(diff(PA),rep(1,mov_mon*2))/(mov_mon*2)),n=len_mon))
+plot(w[,1],type='h')
+lines(w[,2],col=4)
+lines(w[,3],col=2)
