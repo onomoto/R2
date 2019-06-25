6c06e77015ea624dcfa9e434a193894b5727a3ad
onom-gtk-office <onomoto@me.com>
2019-06-25 10:05:41 +0900

    er : add merged plot of actual and theories plus last close price.

--- index/01day.r
+++ index/01day.r
@@ -3,8 +3,10 @@
 
 source(paste(getwd(),"index/getsp5.r",sep='/'),echo=TRUE)
 source(paste(getwd(),"index/nikkei_gspc_jpy.r",sep='/'),echo=TRUE)
-if(index(tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)]) == index(last(to.monthly(SP5))[,c(1,2,3,4,5)])){ 
+if(index(tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)]) == index(last(to.monthly(SP5))[,c(1,2,3,4,5)])){
     tmp.predict[length(index(tmp.predict)),c(1,2,3,4,5)] <- last(to.monthly(SP5))[,c(1,2,3,4,5)]
 }else{
     print("A new month has come. Please update!!!")
 }
+plot(merge(tmp.predict[,c(4,6,7)],rep(as.vector(rep(last(SP5[,4]),length(index(tmp.predict))))))[,c(1,2,3)],type='p',pch='+')
+lines(merge(tmp.predict[,c(4,6,7)],rep(as.vector(rep(last(SP5[,4]),length(index(tmp.predict))))))[,4],col=4)
