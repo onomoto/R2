de56241af8739bb7185cbe8f5fdf86f08f980741
onom-gtk-office <onomoto@me.com>
2019-06-27 16:22:55 +0900

    dc : add lm= parameter.

--- index/experiment/dpdm.r
+++ index/experiment/dpdm.r
@@ -5,7 +5,7 @@ w <- SP5[,4][2:l]/as.vector(SP5[,4][1:l-1])
 dm <- w[diff(cli_xts$oecd)[substr((as.character(index(w))),1,7)] <0]
 length(dm)
 dp <- w[diff(cli_xts$oecd)[substr((as.character(index(w))),1,7)] >0]
-hist(dm,breaks=300,xlim=c(0.95,1.05),col=rgb(0.3,0.6,0,alpha=0.81))
+hist(dm,breaks=300,xlim=c(0.95,1.05),col=rgb(0.3,0.6,0,alpha=0.81),ylim=c(0,600))
 par(new=T)
-hist(dp,breaks=200,xlim=c(0.95,1.05),col=rgb(0,0.3,0.9,alpha=0.5))
+hist(dp,breaks=200,xlim=c(0.95,1.05),col=rgb(0,0.3,0.9,alpha=0.5),ylim=c(0,600))
 t.test(dp,dm)
