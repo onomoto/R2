f07aaea0d924ea440b62782a59fad81a779f019f
onom-gtk-office <onomoto@me.com>
2019-06-27 16:20:26 +0900

    nf : draw overlay histgram of daily return when cli delta plus and minus

---
+++ index/experiment/dpdm.r
@@ -0,0 +1,11 @@
+# draw over lay histogram between daily return on cli delta plus and minus.
+#
+l <- length(index(SP5))
+w <- SP5[,4][2:l]/as.vector(SP5[,4][1:l-1])
+dm <- w[diff(cli_xts$oecd)[substr((as.character(index(w))),1,7)] <0]
+length(dm)
+dp <- w[diff(cli_xts$oecd)[substr((as.character(index(w))),1,7)] >0]
+hist(dm,breaks=300,xlim=c(0.95,1.05),col=rgb(0.3,0.6,0,alpha=0.81))
+par(new=T)
+hist(dp,breaks=200,xlim=c(0.95,1.05),col=rgb(0,0.3,0.9,alpha=0.5))
+t.test(dp,dm)
