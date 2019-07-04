6fafdd2e3489a50b12dd6e679e685b066c21d5a6
onom-gtk-office <onomoto@me.com>
2019-07-04 10:25:44 +0900

    bf : avoid mysterious hist() error. use variables to transfer data from one another.

--- index/experiment/cli_delta_vs_period_return_rate.r
+++ index/experiment/cli_delta_vs_period_return_rate.r
@@ -101,9 +101,15 @@ func <- function(pm="plus",s="1970-01-01",l=1){
 }
 # t_minus <- performance_val
 # t_plus <- performance_val
-func("plus","1970-01-01")
-func("minus","1970-01-01")
+wp <- func("plus","1970-01-01")
+wp
+wm <- func("minus","1970-01-01")
+wm
 # histogram
-hist(as.vector(func("minus","1970-01-01")[,1])-1,col=rgb(0.5,1,0),breaks=20,xlim=c(-0.6,0.5),ylim=c(0,8))
+# hist(as.vector(func("minus","1970-01-01")[,1])-1,col=rgb(0.5,1,0),breaks=20,xlim=c(-0.6,0.5),ylim=c(0,8))
+# par(new=T)
+# hist(as.vector(func("plus","1970-01-01")[,1])-1,col=rgb(0.5,0,1,alpha=0.4),breaks=10,xlim=c(-0.6,0.5),ylim=c(0,8))
+
+hist(as.vector(wm[,1])-1,col=rgb(0.5,1,0),breaks=20,xlim=c(-0.6,0.5),ylim=c(0,8))
 par(new=T)
-hist(as.vector(func("plus","1970-01-01")[,1])-1,col=rgb(0.5,0,1,alpha=0.4),breaks=10,xlim=c(-0.6,0.5),ylim=c(0,8))
+hist(as.vector(wp[,1])-1,col=rgb(0.5,0,1,alpha=0.4),breaks=10,xlim=c(-0.6,0.5),ylim=c(0,8))
