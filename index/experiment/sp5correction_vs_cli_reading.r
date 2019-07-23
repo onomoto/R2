47032ac0d7b9374f28475fa3a0d1edf1b44d7b31
onom-gtk-office <onomoto@me.com>
2019-07-23 12:20:13 +0900

    nf : find price changes in 4 cases. reading more 100 vs. less and delta plus and minus.

---
+++ index/experiment/sp5correction_vs_cli_reading.r
@@ -0,0 +1,66 @@
+
+# delta is parameter relation against month close vs. open, when delta is 0.9.
+# close price is 10% down from open
+# when m is "c", its open vs. close. in the case of "h", it is high versus low
+# this program classifies months into 4 groups. 
+#   1) cli is more than 100 and delta is plus as "dp"
+#   2) cli is more than 100 and delta is minus as "pm"
+#   3) less than 100 and plus as "mp"
+#   4) less than 100 and minus as "mm"
+#
+func <- function(delta=0.9,m="c",l=5){
+  lag_month <- l
+  ind_bp <- index(na.omit(diff(cli_xts$oecd,lag=lag_month))["1962::"])[cli_xts$oecd["1962::"] >100 & na.omit(diff(cli_xts$oecd,lag=1))["1962::"] > 0]
+
+
+  ind_bm <- index(na.omit(diff(cli_xts$oecd,lag=lag_month))["1962::"])[cli_xts$oecd["1962::"]  <100  & na.omit(diff(cli_xts$oecd,lag=1))["1962::"] < 0]
+
+  ind_mp <- index(na.omit(diff(cli_xts$oecd,lag=lag_month))["1962::"])[cli_xts$oecd["1962::"]  <100  & na.omit(diff(cli_xts$oecd,lag=1))["1962::"] > 0]
+
+  ind_pm <- index(na.omit(diff(cli_xts$oecd,lag=lag_month))["1962::"])[cli_xts$oecd["1962::"] >100  & na.omit(diff(cli_xts$oecd,lag=1))["1962::"] < 0]
+
+# sp_correction_ind <- index(SP5["1962::"][SP5["1962::"][,4] / SP5["1962::"][,1] < delta])
+# sp_correction_ind <- index(SP5["1962::"][SP5["1962::"][,3] / SP5["1962::"][,2] < delta])
+
+  # if(c == "h"){ print("T")}
+  # if(c == "o"){ print("S")}else{print("F")}
+  sp_correction_ind <- c()
+  a <- m
+  # switch(a,               # switch(文字列,
+  #   "h" = append(sp_correction_ind,index(SP5["1962::"][SP5["1962::"][,3] / SP5["1962::"][,2] < delta])),
+  #   "c" = append(sp_correction_ind,index(SP5["1962::"][SP5["1962::"][,4] / SP5["1962::"][,1] < delta])),
+  #   print("?")            #  一致するものが
+  # )
+  if(a == "h"){
+    sp_correction_ind <- as.Date(index(to.monthly(SP5)["1962::"][to.monthly(SP5)["1962::"][,3] / to.monthly(SP5)["1962::"][,2] < delta]))
+  }
+  else if(a == "c"){
+    sp_correction_ind <- as.Date(index(to.monthly(SP5)["1962::"][to.monthly(SP5)["1962::"][,4] / to.monthly(SP5)["1962::"][,1] < delta]))
+  }
+  else{
+    print("?")
+  }
+
+# cat("sp_corr ");print(sp_correction_ind)
+  cat("bp ");print(length(ind_bp))
+  cat("bm ");print(length(ind_bm))
+  cat("mp ");print(length(ind_mp))
+  cat("pm ");print(length(ind_pm))
+  # cat("pm+bm+mp ");print(length(append(append(ind_pm,ind_bm),ind_mp)))
+  cat("pm+bm+mp ");print(length(c(ind_pm,ind_bm,ind_mp)))
+
+
+  cat("correction sp5 vs. bm");print(sp_correction_ind[is.element(sp_correction_ind,ind_bm)])
+
+  cat("correction sp5 vs. bp");print(sp_correction_ind[is.element(sp_correction_ind,ind_bp)])
+  cat("correction sp5 vs. pm");print(sp_correction_ind[is.element(sp_correction_ind,ind_pm)])
+  cat("correction sp5 vs. mp");print(sp_correction_ind[is.element(sp_correction_ind,ind_mp)])
+
+
+# t.test(as.vector(VIX[,4][ind_bp]),as.vector(VIX[,4][ind_bm]))
+# print("######################################")
+# t.test(as.vector(VIX[,4][ind_bp]),as.vector(VIX[,4][append(append(ind_pm,ind_bm),ind_mp)]))
+# # t.test(as.vector(VIX[,4][ind_bp]),as.vector(VIX[,4][ind_bm]))
+
+}
+func(0.9,"c",5)
