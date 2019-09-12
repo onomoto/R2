0f1f62f714602ec401c479e9238b009bcf060b55
onom-gtk-office <onomoto@me.com>
2019-09-12 14:23:47 +0900

    dc : remove theory and push forward start_date

--- index/experiment/log_spx_act_theory_ggplot_continuous_scale.r
+++ index/experiment/log_spx_act_theory_ggplot_continuous_scale.r
@@ -4,21 +4,22 @@ func <- function(x){
 
 }
 
-delta <- append(as.vector(diff(cli_xts$oecd)[paste(as.Date(head(index(tmp.predict),1)),"::",sep="")]),rep(NA,length(index(tmp.predict)) - length(diff(cli_xts$oecd)[paste(as.Date(head(index(tmp.predict),1)),"::",sep="")])))
+spx_mean <- apply.monthly(SP5[,4],mean)["1995::"]
+delta <- append(as.vector(diff(cli_xts$oecd)[paste(as.Date(head(index(spx_mean),1)),"::",sep="")]),rep(NA,length(index(spx_mean)) - length(diff(cli_xts$oecd)[paste(as.Date(head(index(spx_mean),1)),"::",sep="")])))
 
 
 df <- data.frame(
-  i=log(as.vector(tmp.predict[,4])),
-  g=log(as.vector(tmp.predict[,6])),
-  e=log(as.vector(tmp.predict[,7])),
-  r=as.vector(tmp.predict[,4]-tmp.predict[,7]),
+  i=log(as.vector(spx_mean)),
+  # g=log(as.vector(tmp.predict[,6])),
+  # e=log(as.vector(tmp.predict[,7])),
+  # r=as.vector(tmp.predict[,4]-tmp.predict[,7]),
   c=as.vector(apply(matrix(delta,ncol=1),1,func)),
-  t=as.Date(index(tmp.predict)))
+  t=as.Date(index(spx_mean)))
 colnames(df)[1] <- 'i'
-colnames(df)[2] <- 'g'
-colnames(df)[3] <- 'e'
-colnames(df)[4] <- 'r'
-colnames(df)[5] <- 'clidelta'
+# colnames(df)[2] <- 'g'
+# colnames(df)[3] <- 'e'
+# colnames(df)[4] <- 'r'
+colnames(df)[2] <- 'clidelta'
 
 p <- ggplot(df,aes(x=t))
 #
@@ -30,14 +31,15 @@ p <- p + geom_path(aes(y=i,color=clidelta),stat="identity", position="identity",
 p <- p + geom_point(aes(y=i,color=clidelta),stat="identity", position="identity",size=3)
 p <- p + scale_x_date(date_breaks = "2 year", date_labels = "%Y")
 # same as above about mapping=
-p <- p + geom_path(aes(y=g),colour='red')
-p <- p + geom_path(aes(y=e),colour='blue')
+# p <- p + geom_path(aes(y=g),colour='red')
+# p <- p + geom_path(aes(y=e),colour='blue')
 # s <- seq(1000,3500,500)
-s <- seq((1+floor(min(tmp.predict[,c(4,6,7)])/500))*500,floor(max(tmp.predict[,c(4,6,7)])/500)*500,500)
+s <- seq((1+floor(min(spx_mean)/500))*500,floor(max(spx_mean/500))*500,500)
 p <- p + geom_hline(yintercept = log(s),size=0.4,linetype=1,colour="white") #horizontal line
 p <- p + geom_hline(yintercept = log(s+250),size=0.4,linetype=2,colour="white") #horizontal line
-p <- p + geom_vline(xintercept=seq(as.Date("2001-01-01"),as.Date("2019-01-01"),by='years'), colour="white",size=0.4)
-for( i in s){ p <- p+annotate("text",label=as.character(s),x=as.Date("2000-01-01"), y=log(s*1.03),colour='white')}
+p <- p + geom_vline(xintercept=seq(as.Date(paste(substr(index(head(spx_mean,1)),1,7),"-01",sep="")),as.Date("2019-01-01"),by='years'), colour="white",size=0.4)
+for( i in s){ p <- p+annotate("text",label=as.character(s),x=as.Date(paste(substr(index(head(spx_mean,1)),1,7),"-01",sep=""))
+, y=log(s*1.03),colour='white')}
 # p <- p + theme(plot.background = element_rect(fill = "darkblue"))
 # p <- p + theme_classic()
 # p <- p +  theme_grey()
