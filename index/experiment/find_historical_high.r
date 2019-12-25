69c79d471cb4faad1eafe26aed7129d3b4481ce7
onom-gtk-office <onomoto@me.com>
2019-12-25 16:48:03 +0900

    dc : detect breakback point before another new high.

--- index/experiment/find_historical_high.r
+++ index/experiment/find_historical_high.r
@@ -1,13 +1,15 @@
 historical_high <- c()
 historical_high <- as.vector(SP5[,4][1])
 historical_high_breakthrough_record <- c()
-period <- "1970-01-01::2019-03-31"
+breakback_record <- c()
+period <- "1970-01-01::2019-10-30"
 
 for(i in seq(2,length(index(SP5[period])),1)) {
   # historical_high <- as.vector(SP5[period][,4][1])
   if(SP5[period][,4][i] > as.vector(last(historical_high))) {
     # cat("high!")
     historical_high <-  append(historical_high,as.vector(SP5[period][,4][i]))
+    breakback_record <- append(breakback_record,1)
     # cat("high2!")
     historical_high_breakthrough_record <- append(historical_high_breakthrough_record,as.xts(as.vector(diff(cli_xts$oecd)[substr(index(SP5[period][,4][i]),1,7)]),index(SP5[period][i])))
     # cat("high3!")
@@ -17,6 +19,12 @@ for(i in seq(2,length(index(SP5[period])),1)) {
     }
     # cat("high4!")
   }else{
+    
+    if(SP5[period][,4][i] < 0.95*as.vector(last(historical_high))){
+      breakback_record[length(breakback_record)] <- 0
+      # cat(SP5[period][,4][i])
+    }
+      
     # cat("low1!")
     historical_high <-  append(historical_high,last(historical_high))
     if((i %% 250) == 0){
@@ -25,4 +33,4 @@ for(i in seq(2,length(index(SP5[period])),1)) {
     }
   }
 }
-historical_high_breakthrough_record
+merge(historical_high_breakthrough_record,breakback_record)
