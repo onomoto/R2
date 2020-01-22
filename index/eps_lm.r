f3af563873c469ea241e6c52d68544db8101bfbd
onom-gtk-home <onomoto@me.com>
2020-01-22 11:07:10 +0900

    dc : add new model <g>

--- index/eps_lm.r
+++ index/eps_lm.r
@@ -52,7 +52,12 @@ func <- function(k="2000-01-01::2018-12-31" ,l=5){
 
   result.gpuc <<- lm(apply.quarterly(SP5[k2k],mean)[,1] ~ PAq[k2k] * UCq[k2k] * G[k2k]*CSq[k2k] - UCq[k2k] -G[k2k] - PAq[k2k]*G[k2k] - UCq[k2k]*G[k2k]*CSq[k2k])
 
-  SP5.result <<- merge(residuals(result.gpuc),predict(result.gpuc),residuals(result.eps),predict(result.eps))
+  result.gpu <<- lm(apply.quarterly(SP5[k2k],mean)[,1] ~
+  +PAq[k2k] * UCq[k2k] +PAq[k2k]  * G[k2k] +UCq[k2k]  * G[k2k] +apply.quarterly(diff(cli_xts$oecd,lag=lag_month)[k2k],mean) - UCq[k2k] - PAq[k2k])
+
+
+
+  SP5.result <<- merge(residuals(result.gpu),predict(result.gpu),residuals(result.eps),predict(result.eps))
   # GSPC.predict <<- merge(to.monthly(GSPC)[substr(k2k,11,23)],last(spline(seq(1,length(SP5.result[,1]),1),as.vector(SP5.result[,2]),n=length(SP5.result[,1])*3-2)$y,n=length(to.monthly(GSPC)[,1][substr(k2k,11,23)])),last(spline(seq(1,length(SP5.result[,1]),1),as.vector(SP5.result[,4]),n=length(SP5.result[,1])*3-2)$y,n=length(to.monthly(GSPC)[,1][substr(k2k,11,23)])),suffixes=c('','spline','eps'))
 
   GSPC.predict <<- merge(to.monthly(SP5)[k2k],
