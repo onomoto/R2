5aef1ed6c38bd5cda5725cbb1bd548753ed1b983
onom-gtk-home <onomoto@me.com>
2021-03-23 09:23:29 +0900

    nf :  daily routine

---
+++ covid/daily.r
@@ -0,0 +1,6 @@
+source("../../Dropbox/R-script/covid/tokyo_effective_repro.r")
+source("../../Dropbox/R-script/covid/em_region.r")
+# source("../../Dropbox/R-script/covid/tokyo_age_split.r")
+source("../../Dropbox/R-script/covid/plot_effective_repro.r")
+
+
