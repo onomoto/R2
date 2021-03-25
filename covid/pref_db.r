efd93ee2d5cb5b0da0c2ed05a9bea6d3fae66262
onom-gtk-home <onomoto@me.com>
2021-03-25 22:37:05 +0900

    dc : change to read data as integer.

--- covid/pref_db.r
+++ covid/pref_db.r
@@ -1,8 +1,12 @@
 # go to https://www.e-stat.go.jp/dbview?sid=0003312316
 # choose csv with cross summary w/ header
 
+# remove comma in an original file.
+# must use gsed not sed.
+system("cat ~/Downloads/c01.csv | gsed 's/\"//g' > ~/Downloads/c02.csv")
 # w <- read.csv("~/Downloads/c01.csv",fileEncoding = "shift_jis")
-pref_db <- cbind(as.data.frame(pref_lab),pref_en,read.csv("~/Downloads/c01.csv",fileEncoding = "shift_jis")[3:49,c(8,10,11,12,13)])
+# read csv. use file encoding to set char code. coClasses to set data type and skip to set # of line to skip.
+pref_db <- cbind(as.data.frame(pref_lab),pref_en,read.csv("~/Downloads/c02.csv",fileEncoding = "shift_jis",colClasses = c(rep(NA,9),rep("integer",4)),skip=1)[3:49,c(8,10,11,12,13)])
 # pref_db <- pref_db[,-4]
 colnames(pref_db)[c(3,4,5,6,7)] <- c("pref_jp","x2010","x2015","x2016","x2017")
 # otherwise use column name
