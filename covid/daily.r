49234c7e46e339cc16f3acb037b3dd9b76322fc8
onom-gtk-home <onomoto@me.com>
2021-04-12 11:04:25 +0900

    dc : change due to toyo keizai halt to provide data

--- covid/daily.r
+++ covid/daily.r
@@ -1,5 +1,5 @@
+source("~/R/R2/covid/em_nhk.r")
 source("../../Dropbox/R-script/covid/tokyo_effective_repro.r")
-source("../../Dropbox/R-script/covid/em_region.r")
+# source("../../Dropbox/R-script/covid/em_region.r")
 # source("../../Dropbox/R-script/covid/tokyo_age_split.r")
 source("../../Dropbox/R-script/covid/plot_effective_repro.r")
-source("~/R/R2/covid/em_nhk.r")
