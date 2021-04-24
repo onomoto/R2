4d1bf02c3157aba2b60500c0919f80ca606c666a
onom-gtk-home <onomoto@me.com>
2021-04-24 11:56:37 +0900

    dc : use private palette

--- covid/em_nhk.r
+++ covid/em_nhk.r
@@ -2,6 +2,7 @@ l <- 4.76
 d <- l+2
 nod <- 7
 
+my_palette <- colorRampPalette(c("#FF0000","#FFFF00","#00FF00","#00FFFF","#0000FF"))
 pref_en <- c("01Hokkaido","02Aomori","03Iwate","04Miyagi","05Akita","06Yamagata","07Fukushima","08Ibaraki","09Tochigi","10Gunma","11Saitama","12Chiba","13Tokyo","14Kanagawa","15Niigata","16Toyama","17Ishikawa","18Fukui","19Yamanashi","20Nagano","21Gifu","22Shizuoka","23Aichi","24Mie","25Shiga","26Kyoto","27Osaka","28Hyogo","29Nara","30Wakayama","31Tottori","32Shimane","33Okayama","34Hiroshima","35Yamaguchi","36Tokushima","37Kagawa","38Ehime","39Kochi","40Fukuoka","41Saga","42Nagasaki","43Kumamoto","44Oita","45Miyazaki","46Kagoshima","47Okinawa")
 pref_jp <- c("北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県")
 pref_lab <- c("Hokkaido","Aomori","Iwate","Miyagi","Akita","Yamagata","Fukushima","Ibaraki","Tochigi","Gunma","Saitama","Chiba","Tokyo","Kanagawa","Niigata","Toyama","Ishikawa","Fukui","Yamanashi","Nagano","Gifu","Shizuoka","Aichi","Mie","Shiga","Kyoto","Osaka","Hyogo","Nara","Wakayama","Tottori","Shimane","Okayama","Hiroshima","Yamaguchi","Tokushima","Kagawa","Ehime","Kochi","Fukuoka","Saga","Nagasaki","Kumamoto","Oita","Miyazaki","Kagoshima","Okinawa")
@@ -69,8 +70,11 @@ g <- ggplot(df, aes(x = t, y = value, fill = variable))
 g <- g + geom_bar(stat = "identity")
 # add labels to print regions names properly in legend.
 # g <- g + scale_fill_hue(name='regions',labels=as.character(unique(w[,3])))
-g <- g + scale_fill_hue(name='regions',labels=pref_jp)
-# g <- g + scale_fill_manual(name='regions',values=rainbow(47))
+# g <- g + scale_fill_hue(name='regions',labels=pref_jp)
+# g <- g + scale_fill_manual(name='regions',values=rep(my_palette(12),4)[-48],labels=pref_jp)
+g <- g + scale_fill_manual(name='regions',values=my_palette(47),labels=pref_jp)
+# g <- g + scale_fill_hue(name='regions',labels=pref_jp)
+# g <- g + scale_color_discrete(name='regions',values=rep(my_palette(6),8)[-48] ,lables=pref_jp)
 g <- g + guides(fill = guide_legend(reverse = F,order = 2),label = TRUE)
 g <- g + theme_gray (base_family = "HiraKakuPro-W3")
 # plot(g)
@@ -170,3 +174,8 @@ d=as.data.frame(as.vector(round(100*apply(last(dmdf[,-48],30),2,sum) / pref_pop,
 
 colnames(mdf) <- c(pref_en,"t")
 colnames(dmdf) <- c(pref_en,"t")
+
+source("../../Dropbox/R-script/covid/tokyo_effective_repro.r")
+# source("../../Dropbox/R-script/covid/em_region.r")
+# source("../../Dropbox/R-script/covid/tokyo_age_split.r")
+source("../../Dropbox/R-script/covid/plot_effective_repro.r")
