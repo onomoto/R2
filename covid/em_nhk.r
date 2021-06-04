95557079998a8c1f4274de1af21c539fb3e8fefe
onom-gtk-home <onomoto@me.com>
2021-06-04 09:47:19 +0900

    dc : fix comment

--- covid/em_nhk.r
+++ covid/em_nhk.r
@@ -177,5 +177,5 @@ colnames(dmdf) <- c(pref_en,"t")
 
 source("../../Dropbox/R-script/covid/tokyo_effective_repro.r")
 # source("../../Dropbox/R-script/covid/em_region.r")
-# source("../../Dropbox/R-script/covid/tokyo_age_split.r")
-source("../../Dropbox/R-script/covid/plot_effective_repro.r")
+source("../../Dropbox/R-script/covid/tokyo_age_split.r")
+# source("../../Dropbox/R-script/covid/plot_effective_repro.r")
